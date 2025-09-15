3f9d4cfc42fdf4356a25649affa6a12f439ede1f
ashwinambatwariitm <24f2009283@ds.study.iitm.ac.in>
2025-09-15 16:06:23 +0200

    Initial commit with README and basic main.cpp

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 24F2009283_git_challenge_lab
 A simple C++ project demonstrating basic arithmetic operations
+This project may contain other arithmatic file one by one as project progresses.
---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file main.cpp
+ * @brief Entry point for the Arithmetic C++ project.
+ *
+ * @author Ashvin Ambatwar
+ * @version 1.0
+ * @date 2025-09-15
+ */
+
+#include <iostream>
+int main() {
+    std::cout << "Arithmetic Project Started!" << std::endl;
+    return 0;
+}
